713b484bf5ec214f49fdc2dffe66f370dd8d0704
vrMars <vrMars@users.noreply.github.com>
2017-08-23 13:18:43 -0400

    basic textures + colors

--- level1.lua
+++ level1.lua
@@ -80,6 +80,13 @@ function scene:create( event )
 	        if (rotation ~= 0) then
 	        	local rect = display.newRect(math.random(10,screenW), screenH/2, xVal, yVal)
 	        	rect.rotation = rotation;
+
+	        	--generate random rect colors (TODO: different colors mean different densities later on)
+	        	local randRed  = math.random(0,1)
+	        	local randGreen  = math.random(0,1)
+	        	local randBlue  = math.random(0,1)
+
+	        	rect:setFillColor(randRed,randGreen,randBlue)
 	        	physics.addBody( rect, "static")
 	        	function rect:touch( event )
 				 if event.phase == "began" then
@@ -134,14 +141,16 @@ function scene:create( event )
 	)
 	
 	-- make a crate (off-screen), position it, and rotate slightly
-	local crate = display.newCircle(90, 90, 10)
+	local crate = display.newCircle(90, 90, 20)
 	crate.rotation = 0
+
+	crate.fill = {type="image", filename="ball.png"}
 	
 	-- add physics to the crate
-	physics.addBody( crate, { density=0.1, bounce=1, friction=0.5, radius=10} )
+	physics.addBody( crate, { density=0.1, bounce=1, friction=0.5, radius=20} )
 	
 	-- create a grass object and add physics (with custom shape)
-	local grass = display.newImageRect( "grass.png", screenW, 82 )
+	local grass = display.newImageRect( "ground.png", screenW, 82 )
 	grass.anchorX = 0
 	grass.anchorY = 1
 	grass.x, grass.y = 0, display.contentHeight
